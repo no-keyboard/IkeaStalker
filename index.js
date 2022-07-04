fbd4ceb7312e1a31fe06c072a6e675b8dec5cd35
no-keyboard <finaldestiny92@gmail.com>
2022-07-04 14:54:29 -0400

    Update index.js

--- index.js
+++ index.js
@@ -9,42 +9,16 @@ const channel = process.env.CHANNEL;
 const errorLog = process.env.ERROR_CHANNEL;
 const tagUser = process.env.TAG_USER;
 const url = process.env.URL;
-const urlList = url.split(";");
-const stores = process.env.STORES;
-const storesToCheck = stores.split(";");
+const zipCode = process.env.ZIP_CODE;
 
-const createEmbed = (name, url, stores) => {
-	const inventoryFields = []
-
-	stores.forEach(el => {
-		inventoryFields.push({
-			name: el.store,
-			value: el.inventory,
-			inline: true
-		});
-	});
-
+const createEmbed = (name, url) => {
 	return {
 		embed: {
 			title: name,
 			url,
-			fields: inventoryFields,
 			timestamp: new Date()
 		}
 	}
-}
-
-const urlListEmbed = urls => {
-	let fields = [];
-
-	urlList.forEach((url, i) => {
-		fields.push({
-			name: `URL ${i + 1}`,
-			value: url
-		});
-	});
-
-	return fields;
 }
 
 bot.login(discordApi);
@@ -70,65 +44,40 @@ puppeteer.launch({
 	await context.overridePermissions(url, ['geolocation'])
 	await page.setGeolocation({latitude: 41.308273, longitude: -72.927879})
 
-	for(let url of urlList) {
-		try {
-			console.log(`Going to ${url}`);
-			await page.goto(url);
-			console.log(`Successfully navigated to ${url}`);
-			await page.waitForSelector("#usdh-availability-cash-and-carry-section");
+	try {
+		console.log(`Going to ${url}`);
+		await page.goto(url);
+		console.log(`Successfully navigated to ${url}`);
+		await page.waitForSelector("div.reservations-container");
+		await page.waitForSelector("#js-location");
 
-			await page.evaluate(() => {
-				const storeListOpen = Array.from(document.querySelectorAll('a')).find(el => el.innerText === "check other IKEA stores");
-				storeListOpen.click();
-			});
+		const result = await page.evaluate(() => {
+			const locations = document.querySelector("#js-location").options;
+			return Array.from(locations);
+		});
 
-			await page.waitForSelector("div.usdh-availability-modal-body");
-			await page.waitForSelector("div.usdh-availability-store-search");
+		console.log(result);
 
-			result = await page.evaluate(storesToCheck => {
-				const productTitle = document.querySelector("span.pip-header-section__title--big").innerText + " ";
-				const productDesc = document.querySelector("span.pip-header-section__description-text").innerText;
-				const product = {
-					product: productTitle + productDesc,
-					url: window.location.href,
-					stores: []
-				}
-				let storesChecked = [];
-				const storeList = document.querySelectorAll("div.usdh-availability-store-information-card");
+		let locationFound = false;
 
-				for(store of storeList) {
-					if(store.querySelector("h3 span")) {
-						console.log(store);
-						const storeName = store.querySelector("h3 span").innerText;
-						const storeInventory = store.querySelector("span").innerText;
-						if(storesToCheck.includes(storeName)) {
-							product.stores.push({
-								store: storeName,
-								inventory: storeInventory
-							});
-						}
-					}
-
-					if(storesChecked.length === storesToCheck.length) {
-						break;
-					}
-				}
-
-				return product;
-			}, storesToCheck);
-
-			console.log(result);
-
-			for(let store of result.stores) {
-				if(store.inventory != "Out of stock" && store.inventory != "Not available at this location") {
-					bot.channels.cache.get(channel).send(createEmbed(result.product, result.url, result.stores));
+		for(location of locations) {
+			console.log(location);
+			try {	
+				if(location.dataset.address.includes(zipCode)) {
+					locationFound = true;
 					break;
 				}
+			} catch(err) {
+				console.error('No dataset');
 			}
-		} catch(err) {
-			console.error(err);
-			// bot.channels.cache.get(errorLog).send(err);
 		}
+
+		if(locationFound) {
+			bot.channels.cache.get(channel).send(createEmbed(`Restaurant found for ${zipCode}!`, url));
+		}
+	} catch(err) {
+		console.error(err);
+		// bot.channels.cache.get(errorLog).send(err);
 	}
 
 	await browser.close();
